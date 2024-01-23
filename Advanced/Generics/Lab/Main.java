3ef2f2773ccffe87dd0d346fd880be9a3e6c596a
lyubenov1 <108736166+lyubenov1@users.noreply.github.com>
2024-01-23 22:29:49 +0200

    Add files via upload

---
+++ Advanced/Generics/Lab/Main.java
@@ -0,0 +1,18 @@
+package Advanced.Generics.Lab;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        Collections.addAll(integers, 1, 2, 18, 2, -1);
+
+        List<String> strings = new ArrayList<>();
+        Collections.addAll(strings, "a", "b", "c");
+
+        String minString = ListUtils.getMin(strings);
+        System.out.println(minString);
+    }
+}
