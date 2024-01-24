d0c2de1df4f6c60d22422d3151a889887fff580e
lyubenov1 <108736166+lyubenov1@users.noreply.github.com>
2024-01-24 14:49:40 +0200

    Add files via upload

---
+++ Advanced/IteratorsAndComparators/Ex/softuni/moviePlatform/MovieBudgetComparator.java
@@ -0,0 +1,10 @@
+package Advanced.IteratorsAndComparators.Ex.softuni.moviePlatform;
+
+import java.util.Comparator;
+
+public class MovieBudgetComparator implements Comparator<Movie> {
+    @Override
+    public int compare(Movie first, Movie second) {
+        return first.getBudget() - second.getBudget();
+    }
+}
---
+++ Advanced/IteratorsAndComparators/Ex/softuni/moviePlatform/MovieNameComparator.java
@@ -0,0 +1,10 @@
+package Advanced.IteratorsAndComparators.Ex.softuni.moviePlatform;
+
+import java.util.Comparator;
+
+public class MovieNameComparator implements Comparator<Movie> {
+    @Override
+    public int compare(Movie first, Movie second) {
+        return first.getName().compareTo(second.getName());
+    }
+}
