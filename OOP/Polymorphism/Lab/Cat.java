b1e9ae8f337eba9f59416df14190411736724f90
lyubenov1 <108736166+lyubenov1@users.noreply.github.com>
2024-02-05 16:43:39 +0200

    Add files via upload

---
+++ OOP/Polymorphism/Lab/Cat.java
@@ -0,0 +1,11 @@
+package OOP.Polymorphism.Lab;
+
+public class Cat extends Animal{
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "MEEOW";
+    }
+}
