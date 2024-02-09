085f3ca708dacbd8a34b5c85b8758085b11b53ce
lyubenov1 <108736166+lyubenov1@users.noreply.github.com>
2024-02-10 01:22:33 +0200

    Add files via upload

---
+++ OOP/TestDrivenDevelopment/Lab/Chainblock/src/main/java/Transaction.java
@@ -0,0 +1,10 @@
+public interface Transaction {
+    int getId();
+    TransactionStatus getStatus();
+
+    void setStatus(TransactionStatus newStatus);
+
+    double getAmount();
+
+    String getFrom();
+}
